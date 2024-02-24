5469a40ac0bf0290ae8e463fd87262b39003116e
Thirumala Makkena <thirumalathiru2003@gmail.com>
2024-02-24 20:17:22 +0530

    ok9

--- index.js
+++ index.js
@@ -13,24 +13,6 @@ const sqlite3 = require("sqlite3");
 app.use(express.json());
 
 
-app.use(function (req, res, next) {
-
-  // Allow all domains to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 
 const path = require("path");
 
@@ -232,7 +214,7 @@ app.get("/getTrackingData/:courierID", async (request, response) => {
   const { courierID } = request.params;
   const query = `
     SELECT 
-        *
+      *
     FROM 
        tracking_history
     WHERE 
