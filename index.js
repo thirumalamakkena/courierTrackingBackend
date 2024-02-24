018f86cb64e72db85312f14b16504c90e625a6f3
Thirumala Makkena <thirumalathiru2003@gmail.com>
2024-02-24 17:31:57 +0530

    ok5

--- index.js
+++ index.js
@@ -12,12 +12,34 @@ const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 app.use(express.json());
 
+
+app.use(function (req, res, next) {
+
+  // Allow all domains to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 const path = require("path");
 
 const dbPath = path.join(__dirname, "courier_tracking.db");
 
 let db = null;
 const port = process.env.PORT || 4000;
+
+
 
 const initializeDBAndServer = async () => {
   try {
