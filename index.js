eb71e056f987d562ee32ebaa8b4e016301205279
Thirumala Makkena <thirumalathiru2003@gmail.com>
2024-02-24 16:47:49 +0530

    ok

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 const express = require("express");
 const app = express();
 
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+
+const path = require("path");
+
+const dbPath = path.join(__dirname, "courier_tracking.db");
+
+let db = null;
+const port = process.env.PORT || 4000;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+   app.listen(port, async () => {
+      console.log(`server is running on port ${port}`);
+    }); 
+    
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+initializeDBAndServer();
+
 app.get("/", (req, res) => {
   res.status(200).send("Langing Page");
 });
@@ -9,7 +37,4 @@ app.get("/about", (req, res) => {
   res.status(200).send("About Page");
 });
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
